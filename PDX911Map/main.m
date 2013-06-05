554c38e69989a1d5d1c7d2067cfd418b90c7e827
Doug Pedley <dpedley@gmail.com>
2013-06-05 11:02:50 -0700

    Initial Commit

---
+++ PDX911Map/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PDX911Map
+//
+//  Created by Douglas Pedley on 6/5/13.
+//  Copyright (c) 2013 dpedley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
